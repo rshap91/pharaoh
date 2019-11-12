b15f10d2761d73cc633d82b85ff91c8627680df3
rshap91 <rick.shapirony@gmail.com>
2019-11-12 17:17:46 -0500

    parse args

--- pharaoh.sh
+++ pharaoh.sh
@@ -5,21 +5,81 @@
 #    wait
 # done
 
-function run_parallel {
-  HOSTFILE=${1}
-  shift
-  SCRIPT=${1}
-  shift
-  ARGS=( $@ )
+get_dirname () {
+  return 'run1'
+}
 
-  echo $HOSTFILE
+many_args () {
+  vals=()
+  while [[ $("$1" | cut -c1) != '-' ]]; do
+    vals+=($1)
+    shift
+  done
+  return vals
+}
+
+
+script_args () {
+  ARGSTRING=''
+  while [[ $("$1" | cut -c1) != '-' ]]; do
+    if [[ "$1" =~ '=' ]]; then
+      flag=`echo $1 | cut -d = -f1`
+      val=`echo$1 | cut -d = -f1`
+      ARGSTRING+=" --${flag} ${val}"
+    else
+      ARGSTRING+=" $1"
+    fi
+}
+
+parse_args () {
+  while [[ $# -gt 0 ]]; do
+    ARG="$1"
+
+    case $ARG in
+      -h | --hosts)
+
+        if [ -f "$2" ]; then
+          HOSTS=( $(<"$2") )
+        else
+          shift
+          HOSTS=`many_args "$@"`
+          continue
+        fi
+        shift 2
+      ;;
+      -s | --script)
+        SCRIPT="$2"
+        shift 2
+      ;;
+      -a | --args)
+        shift
+        SCRIPTARGS+=`script_args "$@"`
+      ;;
+      -k | --keyfile)
+        KEYFILE="$2"
+        shift 2
+      ;;
+      -d | --dirname)
+        DIRNAME='$2'
+        shift 2
+      ;;
+      *)
+        continue
+      ;;
+    esac
+  done
+}
+
+
+run_parallel () {
+
+  SCRIPTARGS=()
+  parse_args "$@"
+
+  echo $HOSTS
   echo $SCRIPT
-  echo $ARGS
-
-  OLDIFS=${IFS}
-  IFS=$'\n'
-  HOSTS=( `<$HOSTFILE` )
-  IFS=$OLDIFS
+  echo $SCRIPTARGS
+  echo $KEYFILE
 
   N=${#HOSTS[@]}
 
@@ -28,9 +88,9 @@ function run_parallel {
     return 1
   fi
 
-  DIR='run1'
-  if [ ! -d $DIR ]; then
-    mkdir $DIR
+  DIRNAME="$DIRNAME" || `get_dirname`
+  if [ ! -d $DIRNAME ]; then
+    mkdir $DIRNAME
   fi
 
 
@@ -39,8 +99,8 @@ function run_parallel {
     user=$(echo $host | cut -d '@' -f 1)
     remote_dir="/home/$user/"
     arg=${ARGS[$i]}
-    scp -i ~/.ssh/amazon.pem "$SCRIPT" "${host}:${remote_dir}"
-    ssh -i ~/.ssh/amazon.pem -n -o BatchMode=yes -o StrictHostKeyChecking=accept-new "$host" "chmod 700 ${remote_dir}$SCRIPT && ${remote_dir}$SCRIPT" "${ARGS[@]}" 1>"$DIR/$host.log" 2>&1 &
+    scp -i $KEYFILE "$SCRIPT" "${host}:${remote_dir}"
+    ssh -i $KEYFILE -n -o BatchMode=yes -o StrictHostKeyChecking=accept-new "$host" "chmod 700 ${remote_dir}$SCRIPT && ${remote_dir}$SCRIPT" "${ARGS[@]}" 1>"$DIRNAME/$host.log" 2>&1 &
   done
   wait
 }
